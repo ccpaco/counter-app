08c88cd48e1990029a1ecbd576099c1edd808366
Christian Champaco <chrischampaco@gmail.com>
2020-10-13 09:50:09 +1000

    Lesson 6, Add jsx elems, pass style to span tag

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,13 +5,21 @@ class Counter extends Component {
         count : 0
     };
 
+//style is a reserv. keyword, styles is cust obj to hold diff (css?) attributes to pass to span
+    styles = {
+        fontSize: 50,
+        fontWeight: "bold" 
+    };
+
 // use js inside span, in this case {function()} 
+//because jsx babel compile, span uses className instead of class (html). Also using Bootstrap!
     render() { 
         return ( 
-        <React.Fragment>
-        <span>{this.formatCount()}</span>
-        <button> Increment</button>
-        </React.Fragment> 
+        <div>
+        
+        <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className ="btn btn-secondary btn-sm"> Increment</button>
+        </div> 
         );
     }
 //jsx expressions are normal js objs
