331fdf9a887e0feb1197fb95d3de18075aaf15bf
Christian Champaco <chrischampaco@gmail.com>
2020-10-12 22:19:47 +1000

    ref. prev. msg

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 
 class Counter extends Component {
+    state = { 
+        count : 0
+    };
+
+// use js inside span, in this case {function()} 
     render() { 
-        return <h1>Hello World</h1>;
+        return ( 
+        <React.Fragment>
+        <span>{this.formatCount()}</span>
+        <button> Increment</button>
+        </React.Fragment> 
+        );
+    }
+//jsx expressions are normal js objs
+    formatCount() {
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
 }
  
