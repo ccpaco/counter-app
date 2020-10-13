fac5340dff3e3dd8758c279aedf5fbf5f1074813
Christian Champaco <chrischampaco@gmail.com>
2020-10-13 11:19:41 +1000

    L7; Use Refactoring of classes for dynamic rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,23 +5,25 @@ class Counter extends Component {
         count : 0
     };
 
-//style is a reserv. keyword, styles is cust obj to hold diff (css?) attributes to pass to span
-    styles = {
-        fontSize: 50,
-        fontWeight: "bold" 
-    };
-
 // use js inside span, in this case {function()} 
 //because jsx babel compile, span uses className instead of class (html). Also using Bootstrap!
     render() { 
+        
+
         return ( 
         <div>
         
-        <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button className ="btn btn-secondary btn-sm"> Increment</button>
         </div> 
         );
     }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
 //jsx expressions are normal js objs
     formatCount() {
         const { count } = this.state;
