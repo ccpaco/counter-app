bef6ba5c09cc6470d00d5a64c031e8b51b6b3925
Christian Champaco <chrischampaco@gmail.com>
2020-10-15 10:24:33 +0900

    L8 & L9: Dyn. Rendering & Conditional Rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,33 +2,26 @@ import React, { Component } from 'react';
 
 class Counter extends Component {
     state = { 
-        count : 0
+        count : 0,
+        tags : ["tag 1", "tag 2", "tag 3"]
     };
 
+    renderTags(){
+        if (this.state.tags.length === 0) return <p> There are no tags! </p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+        
+    }
 // use js inside span, in this case {function()} 
 //because jsx babel compile, span uses className instead of class (html). Also using Bootstrap!
     render() { 
-        
-
         return ( 
         <div>
-        
-        <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-        <button className ="btn btn-secondary btn-sm"> Increment</button>
+            { this.state.tags.length === 0 && 'Please add new tags'}
+            { this.renderTags() }
         </div> 
         );
     }
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-//jsx expressions are normal js objs
-    formatCount() {
-        const { count } = this.state;
-        return count === 0 ? 'Zero' : count;
-    }
+    
 }
  
 export default Counter;
