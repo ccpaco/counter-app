842510b8525e87bf7bc47df89bf6430e2afc9bf6
Christian Champaco <chrischampaco@gmail.com>
2020-10-15 11:33:52 +0900

    Add renderTags(), keep original button helper classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+//Take your time, learn well. There are a lot of diff syntaxes at play here. 10-15-2020
+
 
 class Counter extends Component {
     state = { 
@@ -17,17 +19,26 @@ class Counter extends Component {
 // use js inside span, in this case {function()} 
 //because jsx babel compile, span uses className instead of class (html). Also using Bootstrap!
     render() { 
-        return ( 
-            <div>
-                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
-                <button 
-                onClick={this.handleIncrement} 
-                className ="btn btn-secondary btn-sm"
-                >
-                 Increment
-                </button>
-            </div> 
+        return (
+          <div>
+            <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+            <button
+              onClick={this.handleIncrement}
+              className="btn btn-secondary btn-sm"
+            >
+              Increment
+            </button>
+            {this.renderTags()}
+          </div>
         );
+    }
+
+    renderTags() {
+        return <ul>
+            {this.state.tags.map((tag) => (
+                <li key={tag}>{tag}</li>
+            ))}
+        </ul>;
     }
 
     getBadgeClasses() {
