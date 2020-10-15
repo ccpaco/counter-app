923e94515bc12cdeb42def541498b188d6e50731
Christian Champaco <chrischampaco@gmail.com>
2020-10-15 10:42:12 +0900

    L10 : working on event handlers, re-add button support classes

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,20 +6,39 @@ class Counter extends Component {
         tags : ["tag 1", "tag 2", "tag 3"]
     };
 
-    renderTags(){
-        if (this.state.tags.length === 0) return <p> There are no tags! </p>;
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
-        
+    constructor(){
+        super();
+        console.log("Constructor", this);
+    }
+
+    handleIncrement(){
+        console.log('Increment Clicked', this);
     }
 // use js inside span, in this case {function()} 
 //because jsx babel compile, span uses className instead of class (html). Also using Bootstrap!
     render() { 
         return ( 
-        <div>
-            { this.state.tags.length === 0 && 'Please add new tags'}
-            { this.renderTags() }
-        </div> 
+            <div>
+                <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button 
+                onClick={this.handleIncrement} 
+                className ="btn btn-secondary btn-sm"
+                >
+                 Increment
+                </button>
+            </div> 
         );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+        const { count } = this.state;
+        return count === 0 ? 'Zero' : count;
     }
     
 }
